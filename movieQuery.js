6f0a73a14e3a80377ac95dd9fae471b520bc2c5b
cis101 <cis101@secs.iub.edu.bd>
2017-10-02 09:24:39 +0600

    assignment05

--- movieQuery.js
+++ movieQuery.js
@@ -4,24 +4,30 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  { title: "The Matrix Reloaded", year: "2003", length: 138, rating: "7.2", genre: "sci-fi" },
+  {title:"Grudge",year:"2004",length:165,rating: "8.0",genere:"horror"}
+ 
 ];
 
 function getMovieTitle( index ){
-  //add code
+  return movieList[index].tittle;
 }
 
 console.log(getMovieTitle(3));
+console.log(getMovieTitle(0));
+console.log(getMovieTitle(6));
 
 function addMovie( movie ){
-  //add code
+  movieList.push(movie);
+  return movieList;
+
 }
 
-var newMovie = {};
+var newMovie = {title:"Grudge",year:"2004",length:165,rating: "8.0",genere:"horror"} ;
 console.log(addMovie(newMovie));
 
 function movieByRating(){
-  //add code
+  
 }
 
 console.log(movieByRating());
