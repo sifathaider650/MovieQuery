f4b295528d27ec0a97df2dd0d214e53b0e479bb4
sifathaider650 <32050060+sifathaider650@users.noreply.github.com>
2017-10-18 11:55:59 -0700

    Update movieQuery.js

--- movieQuery.js
+++ movieQuery.js
@@ -5,17 +5,13 @@ var movieList = [
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
   { title: "The Matrix Reloaded", year: "2003", length: 138, rating: "7.2", genre: "sci-fi" },
-  {title:"Grudge",year:"2004",length:165,rating: "8.0",genere:"horror"}
+ { title: "Moana", year: "2017", length: 142, rating: "8.0", genre: "Adventure" }
  
 ];
 
 function getMovieTitle( index ){
   return movieList[index].tittle;
 }
-
-console.log(getMovieTitle(3));
-console.log(getMovieTitle(0));
-console.log(getMovieTitle(6));
 
 function addMovie( movie ){
   movieList.push(movie);
@@ -24,16 +20,37 @@ function addMovie( movie ){
 }
 
 var newMovie = {title:"Grudge",year:"2004",length:165,rating: "8.0",genere:"horror"} ;
+console.log("\n\nAdd movie: \n\n");
 console.log(addMovie(newMovie));
 
 function movieByRating(){
+   var init = [];
+   for(var i=0; i<movieList.length; i++){
+     for(var j=i; j<movieList.length-1; j++){
+       if(movieList[j].rating<movieList[j+1].rating){
+         init=movieList[j];
+         movieList[j]=movieList[j+1];
+         movieList[j+1]=init;
+       }
+     }
+   }
+   movieList.sort(function(a, b){
+     return (b.rating)-(a.rating);
+   });
+    return movieList;
+  }
   
-}
+  console.log("\n\nsorted by ratings in descending order: \n\n");
+  console.log(movieByRating());
 
-console.log(movieByRating());
 
 function findByTitle( title ){
   //add code
+  var list = [];
+  movieList.map(function(v,i){
+    if(v.title.toLowerCase().includes(title)) list.push(v);
+  });
+  return list;
 }
-
+console.log("\n\nMovie found by title: \n\n");
 console.log(findByTitle("matrix"));
